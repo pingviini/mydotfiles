94a10664fc6f89fed56f759a2bec1b486801b218
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2013-02-26 23:38:14 +0200

    Updated readme.

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
-Pingviini's dotfiles
-====================
+Pingviinis dotfiles
+===================
 
 Collection of various dot files.
 
@@ -7,6 +7,7 @@ Install
 =======
 
 ::
+
     $ sudo aptitude install git mercurial
     $ wget -q --no-check-certificate -O - https://github.com/pingviini/mydotfiles/raw/master/install.sh  | bash
 
@@ -16,6 +17,3 @@ Thanks
 
 * Steve Losh for mercurial templates
   http://hgtip.com/tips/advanced/2010-01-15-styling-mercurials-cli/
-
-
-
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Installing pingviini's dotfiles"
-echo "===============================\n"
+echo "Installing dotfiles"
+echo "===================\n"
 
 echo "$(tput setaf 7)Phase 0 - Clone pingviini's mydotfiles repo$(tput setaf 9)"
 echo "-------------------------------------------"
@@ -30,12 +30,12 @@ fi
 if [ ! -f $HOME/.hgignore ]; then
     echo " * symlinking hgignore to $HOME/.hgignore"
     ln -s $HOME/.mydotfiles/mercurial/hgrc $HOME/.hgignore
-    echo " $(tput setaf 2)* symlink ready.$(tput setaf 9)"
+    echo " $(tput setaf 2)* symlink ready.$(tput setaf 9)\n"
 else
-    echo " $(tput setaf 3)* found .hgignore from $HOME.$(tput setaf 9)"
+    echo " $(tput setaf 3)* found .hgignore from $HOME.$(tput setaf 9)\n"
 fi
 
-echo "\n$(tput setaf 7)Phase 2 - Git$(tput setaf 9)"
+echo "$(tput setaf 7)Phase 2 - Git$(tput setaf 9)"
 echo "-------------"
 
 if [ ! -f $HOME/.gitconfig ]; then
